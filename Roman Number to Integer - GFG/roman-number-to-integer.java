2dce8cc7437e1ffe3f99c70bc2a7241146d7c2a6
abhaysingh28355 <147785453+abhaysingh28355@users.noreply.github.com>
2023-10-20 06:31:39 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.java
+++ Roman Number to Integer - GFG/roman-number-to-integer.java
@@ -24,26 +24,27 @@ class GFG {
 
 class Solution {
     // Finds decimal value of a given roman numeral
-    public int romanToDecimal(String str) {
+    public int romanToDecimal(String s) {
         // code here
-        HashMap<Character,Integer>map = new HashMap<>();
-        map.put('I',1);
-         map.put('V',5);
-          map.put('X',10);
-           map.put('L',50);
-            map.put('C',100);
-             map.put('D',500);
-              map.put('M',1000);
-              
-              int result = map.get(str.charAt(str.length()-1));
-              for(int i=str.length()-2;i>=0;i--){
-                  if(map.get(str.charAt(i))< map.get(str.charAt(i+1))){
-                      result -=map.get(str.charAt(i));
-                  }
-                  else{
-                     result +=map.get(str.charAt(i));
-              }
-              }
-              return result;
+          int ans = 0;
+        char c = ' ';
+        HashMap<Character,Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        for(int i=0;i<s.length()-1;i++){
+            c = s.charAt(i);
+            if(m.get(c) >= m.get(s.charAt(i+1))){
+                ans += m.get(c);
+            }else{
+                ans -= m.get(c);
+            }
+        }
+        ans += m.get(s.charAt(s.length()-1));
+        return ans;
     }
 }
